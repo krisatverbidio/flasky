562dab6d7a281d7b6cd277b16b70571fbdb37907
Kristofer Pettijohn <kris@verbidio.com>
2022-12-20 10:31:30 -0600

    output

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from typing import Union
+from typing import Union, List
+from pydantic import BaseModel
+import subprocess
+import uvicorn
+
 
 from fastapi import FastAPI
 import os
 
 app = FastAPI()
 
+
+class Command(BaseModel):
+    command: List[str]
 
 @app.get("/")
 def read_root():
@@ -14,6 +21,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/run/")
+def run_command(command: Command):
+    r = subprocess.run(command.command, shell=True, capture_output=True)
+    output = r.stdout.decode().splitlines()
+    return output
+    # return command
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
